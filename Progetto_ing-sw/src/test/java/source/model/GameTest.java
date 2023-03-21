5d382b534283c89572afda91f3cd755305a79f99
Lorenzo <lorenzo.vercelloni@mail.polimi.it>
2023-03-21 12:22:54 +0100

    GameTest class

---
+++ Progetto_ing-sw/src/test/java/source/model/GameTest.java
@@ -0,0 +1,30 @@
+package source.model;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class GameTest extends TestCase {
+
+    @Test
+    public void testGetCurrentPlayer() {
+        //Checking the current player
+    }
+
+    public void testSetCurrentPlayer() {
+    }
+
+    public void testGetCommonGoals() {
+    }
+
+    public void testGetCommonGoalsPoints() {
+    }
+
+    public void testSetCommonGoals() {
+    }
+
+    public void testUpdatePoints() {
+    }
+
+    public void testEndGameToken() {
+    }
+}
