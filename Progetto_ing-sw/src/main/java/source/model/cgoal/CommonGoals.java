e7a5e7e335f8597d234057302db76b2abee0831f
Arimndo <arimondo.scrivano@mail.polimi.it>
2023-03-21 17:50:17 +0100

    debug CommonGoals interface

--- Progetto_ing-sw/src/main/java/source/model/cgoal/CommonGoals.java
+++ Progetto_ing-sw/src/main/java/source/model/cgoal/CommonGoals.java
@@ -1,6 +1,6 @@
 package source.model.cgoal;
 //abstract class that has the strategy method
-interface CommonGoals{
+ public interface CommonGoals{
 
     // the result are 0 or 1: 0 no match, 1 match
     public int Checker(int[][] matrix);
