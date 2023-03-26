5a824f194808207a3861b2498a537b7c46cf7ff5
Lorenzo <lorenzo.vercelloni@mail.polimi.it>
2023-03-26 17:57:55 +0200

    Update in controller

---
+++ Progetto_ing-sw/src/main/java/source/controller/GameController.java
@@ -0,0 +1,26 @@
+package source.controller;
+
+import source.model.*;
+
+//TODO
+public class GameController {
+    //Model
+    Game currentGame;
+    //View
+    //UI userInterface;
+    //Chat currentChat;
+
+    //Constructor
+    public GameController(Game currentGame /*, UI userInterface*/) {
+        this.currentGame = currentGame;
+        //this.userInterface=userInterface;
+    }
+
+    @Deprecated
+    public void createPlayer(){
+        /*if(currentGame.players.size()< Game.MAX_PLAYERS){
+            Add player to the game
+        }*/
+    }
+
+}
