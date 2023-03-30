fa04ae8667616d9c0a7cd8cf695c705e6adc43bc
Pietro <pietro1.pavesi@mail.polimi.it>
2023-03-30 12:15:19 +0200

    Dashboard: bug fixed

--- Progetto_ing-sw/src/main/java/model/Dashboard.java
+++ Progetto_ing-sw/src/main/java/model/Dashboard.java
@@ -2,23 +2,23 @@ package model;
 
 public class Dashboard {
     //Dashboard model
-    private  Tile[][] tiles;
+    private Tile[][] tiles;
     //Setting the state of a tile
     private TILETYPE[][] refillable;
     private boolean refill;
     //Number of players
     private int players;
 
-    public enum TILETYPE{
-        TWO_PL, THREE_PL, FOUR_PL,BLK
+    public enum TILETYPE {
+        TWO_PL, THREE_PL, FOUR_PL, BLK
     }
 
 
     //Constructor given the number of players
     //TODO
-    public Dashboard(int np, Bag bagInGame){
+    public Dashboard(int np, Bag bagInGame) {
         //correct format
-        np-=1;
+        np -= 1;
 
 
         this.refillable = new TILETYPE[][] {
@@ -34,17 +34,16 @@ public class Dashboard {
         };
 
 
-
         this.tiles = new Tile[9][9];
         //creating the matrix of tiles in the correct spot
 
-        for (int r=0; r< 9; r++) {
-            for (int c=0; c<9; c++) {
-                if (refillable[r][c].ordinal()<np) {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (refillable[r][c].ordinal() < np) {
                     tiles[r][c] = bagInGame.getRandomTile();
 
                 } else if (refillable[r][c].equals(TILETYPE.BLK)) {
-                    tiles[r][c] = new Tile(COLOR.BLANK,0);
+                    tiles[r][c] = new Tile(COLOR.BLANK, 0);
 
 
                 }
@@ -52,33 +51,31 @@ public class Dashboard {
         }
 
 
-
         this.refill = false;
-        this.players = np+1;
+        this.players = np + 1;
     }
 
     //TODO
-    public void updateDashboard(Tile[] pickedTiles) {
+    public void updateDashboard(Tile[] pickedTiles, Bag bagInGame) {
         // After the turn of a player update the dashboard
         // function gets the array composed of one-to-three tails picked by the player
 
         // then searches those tails on the dashboard
-        for (int index = 0; index < 3; index ++) {
+        for (int index = 0; index < 3; index++) {
 
             for (int r = 0; r < 9; r++) {
                 for (int c = 0; c < 9; c++) {
 
                     if (this.tiles[r][c].equals(pickedTiles[index])) {
 
-                        this.tiles[r][c] = new Tile(COLOR.BLANK,0);
-
-                        /* method equals should compare the status, so tail's color and id
-                        *  usato così potrebbe causare problemi nella pick o può andare? */
-
+                        this.tiles[r][c] = new Tile(COLOR.BLANK, 0);
+                        //method equals should compare the status, so tail's color and id
                     }
                 }
             }
         }
+        // checking if dashboard needs to be refilled
+        setRefill(bagInGame);
     }
 
     //TODO
@@ -118,7 +115,7 @@ public class Dashboard {
 
 
     //TODO
-    public void refillDashboard(Bag bagInGame){
+    public void refillDashboard(Bag bagInGame) {
 
         // scanning the dashboard cells
         for (int r = 0; r < 9; r++) {
@@ -133,8 +130,8 @@ public class Dashboard {
     }
 
 
-    public Dashboard getDashboard(){
+    public Dashboard getDashboard() {
         return this;
     }
-}
 
+}
