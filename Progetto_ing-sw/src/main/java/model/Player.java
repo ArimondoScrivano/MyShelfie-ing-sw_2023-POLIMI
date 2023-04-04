2d5dbc0dbfaa59f7493f16bd8d21b1ba3295320a
Rita <mariarita.sartini@mail.polimi.it>
2023-04-04 13:45:40 +0200

    Shelf and player methods changed

--- Progetto_ing-sw/src/main/java/model/Player.java
+++ Progetto_ing-sw/src/main/java/model/Player.java
@@ -2,6 +2,7 @@ package model;
 
 import model.cgoal.CommonGoals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
@@ -56,24 +57,89 @@ public class Player {
         return false;
     }
     //salvare oldChain nella riga dell'adiacenza
-    public int convertPoints(Player myPlayer, int[] chains, int endline){
-        myPlayer.points=0;
-        if(chains[endline]==3){
-            myPlayer.points+=2;
-        }
-        if(chains[endline]==4){
-            myPlayer.points+=3;
-        }
-        if(chains[endline]==5){
-            myPlayer.points+=5;
-        }
-        if(chains[endline]>=6){
-            myPlayer.points+=8;
+    public void convertPoints(Tile[][] matrix) {
+        this.points = 0;
+        int count = 0;
+        // we have to use a support matrix to avoid corner cases and so we can modify the elements
+        Tile[][] matrixSupport = new Tile[8][7];
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 7; c++) {
+
+                if (r > 0 && r < 7 && c > 0 && c < 6) {
+                    matrixSupport[r][c] = new Tile(matrix[r - 1][c - 1].getColor(), 1);
+                } else {
+                    matrixSupport[r][c] = new Tile(COLOR.BLANK, 1);
+                }
+            }
         }
 
-        //TODO SISTEMARE TIPO UML*/
-        return myPlayer.points;
+        for (int color = 1; color < 7; color++) {
+
+            for (int row = 1; row < 7; row++) {
+                for (int col = 1; col < 6; col++) {
+
+                    //check if the tile is the right color
+                    if (matrixSupport[row][col].getColor().compareTo(COLOR.BLANK) == color) {
+                        List<Tile> groupFound = new ArrayList<>();
+                        matrixSupport[row][col] = new Tile(COLOR.BLANK, 1);
+                        groupFound.add(matrixSupport[row][col]);
+
+                        for (int index = 0; index < groupFound.size(); index++) {
+
+                            //search the correct tile
+                            for (int i = 1; i < 7; i++) {
+                                for (int j = 1; j < 6; j++) {
+                                    //found the correct tile
+                                    if (matrixSupport[i][j].equals(groupFound.get(index))) {
+
+                                        //check the adjacency
+                                        //upper-case
+                                        if (matrixSupport[i + 1][j].getColor().compareTo(COLOR.BLANK) == color) {
+                                            matrixSupport[i + 1][j] = new Tile(COLOR.BLANK, 1);
+                                            groupFound.add(matrixSupport[i + 1][j]);
+
+                                        }
+                                        //lower-case
+                                        if (matrixSupport[i - 1][j].getColor().compareTo(COLOR.BLANK) == color) {
+                                            matrixSupport[i - 1][j] = new Tile(COLOR.BLANK, 1);
+                                            groupFound.add(matrixSupport[i - 1][j]);
+
+                                        }
+                                        //right-case
+                                        if (matrixSupport[i][j + 1].getColor().compareTo(COLOR.BLANK) == color) {
+                                            matrixSupport[i][j + 1] = new Tile(COLOR.BLANK, 1);
+                                            groupFound.add(matrixSupport[i][j + 1]);
+
+                                        }
+                                        //left-case
+                                        if (matrixSupport[i][j - 1].getColor().compareTo(COLOR.BLANK) == color) {
+                                            matrixSupport[i][j - 1] = new Tile(COLOR.BLANK, 1);
+                                            groupFound.add(matrixSupport[i][j - 1]);
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (groupFound.size() == 3) {
+                            points = points + 2;
+                        }
+                        if (groupFound.size() == 4) {
+                            points = points + 3;
+                        }
+                        if (groupFound.size() == 5) {
+                            points = points + 5;
+                        }
+                        if (groupFound.size() >= 6) {
+                            points = points + 8;
+                        }
+                    }
+                }
+
+            }
+        }
     }
+
 
     public boolean[] getCommonGoalsCompleted() {
         return commonGoalsCompleted;
--- Progetto_ing-sw/src/main/java/model/Shelf.java
+++ Progetto_ing-sw/src/main/java/model/Shelf.java
@@ -1,81 +1,76 @@
 package model;
 
-import model.command.Command;
-import model.command.ConcreteCommand1;
-import model.command.ConcreteCommand2;
-import model.command.ConcreteCommand3;
-
 import java.io.IOException;
 
 public class Shelf {
     //pattern command per tiles adiacenti
     protected Tile[][] tilesShelf;
-    int[][] tileTypes=new int[6][5];
+    int[][] tileTypes = new int[6][5];
     Player myPlayer;
-    Command myCommand;
+
     //CONSTRUCTOR FOR THE SHELF CLASS
-    public Shelf(Tile[][] tilesShelf, Player myPlayer){
-        this.myPlayer=myPlayer;
-        this.tilesShelf=tilesShelf;
+    public Shelf(Tile[][] tilesShelf, Player myPlayer) {
+        this.myPlayer = myPlayer;
+        this.tilesShelf = tilesShelf;
     }
+
     //METHOD TO RETURN TRUE WHETHER ONE'S SHELF IS COMPLETED
-    public boolean completeShelf(){
-        int i=0, j=0;
-        while(i<5){
-            while(j<6){
-                if(tilesShelf[i][j].getColor().compareTo(COLOR.BLANK)==0){
+    public boolean completeShelf() {
+        int i = 0, j = 0;
+        while (i < 5) {
+            while (j < 6) {
+                if (tilesShelf[i][j].getColor().compareTo(COLOR.BLANK) == 0) {
                     return false;
                 }
             }
         }
         return true;
     }
-    public int addTiles(Tile tiles[], int column) throws IOException {
-        int k=0;
+
+    public void addTiles(Tile tiles[], int column) {
+        int freeFirstSpot = 0;
+        int flag = 0;
         int types[];
-        if(tiles.length>4) System.out.println("too many tiles picked.");
-        int counter=0;
-        for(int j=0; j<=5; j++){
-            if(tileTypes[j][column]==0){
-                counter++;
-            }
-        }if(tiles.length>counter){
-            System.out.println("not enough vacant spaces on the selected column."); //lanciare eccezione?
-        }else{
-            types=new int[tiles.length+1];
-            for(int mover=0; mover<tiles.length; mover++){
-                types[mover]=tiles[mover].getColor().compareTo(COLOR.BLANK);
-            }
-            //System.out.println("ho accettato le tiles scelte");
-            int j=5;
-            while(j>=0){ //ATTENZIONE PERCHè HO INVERTITO TUTTO
-                if(tileTypes[j][column]==0){
-                    //System.out.println("ho trovato il primo spazio libero");
-                    int firstLine=j;
-                    int i=0;
-                    for(i=0; i<tiles.length-1; i++){
-                        tileTypes[j][column]=tiles[i].getColor().compareTo(COLOR.BLANK);//mi ritorna un int
-                        types[i]=tiles[i].getColor().compareTo(COLOR.BLANK);
-                        //System.out.println("ho registrato la tessera"+i);
-                        j--;
-                    }//System.out.println("check del personal goal");
-                    //myPlayer.myPersonalGoal.checkPersonalGoal(myPlayer, tileTypes);
-                    if(i==tiles.length){ j=+1;}//quando mi trovo sulla posizione i=tiles.lenght significa che la jè stata diminuita una volta di troppo
-                    if(column==0) myCommand=new ConcreteCommand1(tileTypes);
-                    if(column==4) myCommand=new ConcreteCommand2(tileTypes);
-                    if(column!=0 && column!=4) myCommand=new ConcreteCommand3(tileTypes);
-                    k=myCommand.adjacentTiles(column, firstLine, types, myPlayer); //trovare il modo di far ritornare oldchains a shelf così che non perdo l'info sul turno precedente
-                    System.out.println(k);
-                    //System.out.println("ho chiamato il comando");
-                    j=-1;
-                    //System.out.println(j);
-                    //System.out.println(k);
-                }else{j--;}
+        if (tiles.length > 4) System.out.println("too many tiles picked.");
+        if (tiles.length == 3) {
+            if (tilesShelf[2][column].getColor().equals(COLOR.BLANK)) {
+                flag = 1;
             }
         }
-        return k;
+        if (tiles.length == 2) {
+            if (tilesShelf[1][column].getColor().equals(COLOR.BLANK)) {
+                flag = 1;
+            }
+        }
+        if (tiles.length == 1) {
+            if (tilesShelf[0][column].getColor().equals(COLOR.BLANK)) {
+                flag = 1;
+            }
+        }
+        for (int col = 1; col < 6; col++) {
+            if (!tilesShelf[col - 1][column].getColor().equals(COLOR.BLANK) && !tilesShelf[col][column].getColor().equals(COLOR.BLANK)) {
+                freeFirstSpot = col;
+            }
+        }
+
+        if (flag == 1) {
+            for (int i = 0; i < tiles.length; i++) {
+                tilesShelf[freeFirstSpot - i][column] = new Tile(tiles[i].getColor(), tiles[i].getId());
+            }
+        }
+        myPlayer.convertPoints(tilesShelf);
+        int[][] matrixSupport = new int[6][5];
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 5; c++) {
+                matrixSupport[r][c] = this.tilesShelf[r][c].getColor().compareTo(COLOR.BLANK);
+            }
+        }
+
+        try {
+            myPlayer.myPersonalGoal.checkPersonalGoal(this.myPlayer, matrixSupport);
+        } catch (IOException e) {
+
+        }
     }
-
 }
-
 //scrivere casi di test
