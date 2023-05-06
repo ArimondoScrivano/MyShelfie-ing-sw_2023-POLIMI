dc601de3626c92c23371b3977c5958a116c2cf77
Lorenzo <lorenzo.vercelloni@mail.polimi.it>
2023-05-06 17:07:51 +0200

    Cli clean up

--- Progetto_ing-sw/src/main/java/view/Cli.java
+++ Progetto_ing-sw/src/main/java/view/Cli.java
@@ -24,7 +24,7 @@ public class Cli{
 
     //Asking the nickname
     public String askNickname() {
-        out.println("Choose your nickname: ");
+        out.print("Choose your nickname: ");
         //Checking if the name is not the same as other players in game
         return in.nextLine();
     }
@@ -37,10 +37,10 @@ public class Cli{
 
 
     public int askNumberOfPlayers(){
-        out.println("Select the number of players between 2 and 4");
+        out.print("Select the number of players between 2 and 4 ");
         int numberOfPlayers=in.nextInt();
         while(numberOfPlayers<2 || numberOfPlayers>4){
-            System.out.println(ColorUI.RED_TEXT+"You must choose a number of players between 2 and 4! Retry"+ColorUI.RESET);
+            System.out.print(ColorUI.RED_TEXT+"You must choose a number of players between 2 and 4! Retry "+ColorUI.RESET);
             numberOfPlayers=in.nextInt();
         }
         return numberOfPlayers;
@@ -48,10 +48,10 @@ public class Cli{
 
     //Asking the number of tiles to pick
     public int askNumberOfTiles(){
-        out.println("Chose the number of tiles that you want to pick: ");
+        out.print("Chose the number of tiles that you want to pick: ");
         int numberOfTile=in.nextInt();
         while(numberOfTile<1 || numberOfTile>3){
-            out.println(ColorUI.RED_TEXT+"You must choose a number between 1 and 3! Retry"+ColorUI.RESET);
+            out.print(ColorUI.RED_TEXT+"You must choose a number between 1 and 3! Retry "+ColorUI.RESET);
             numberOfTile=in.nextInt();
         }
         return numberOfTile;
@@ -61,23 +61,23 @@ public class Cli{
     public List<Integer> askTilesToPick(int numberOfTile){
         //Se 3 tile da prendere -->lista con 6 elementi
         List<Integer> tilesToPick = new ArrayList<>();
-        out.println("Choose the tiles to pick using x and y coordinates: ");
+        out.println("Choose the tiles to pick using x and y coordinates");
         for(int i=0; i<numberOfTile*2; i++){
             if(i%2==0){
-                out.println("Chose the x coordinate of the tile that you want to pick: ");
+                out.print("Chose the x coordinate of the tile that you want to pick: ");
                 int x=in.nextInt();
                 //Check if the value is between 1 and 10
                 while(x<0 || x>11){
-                    out.println(ColorUI.RED_TEXT+"You must choose a number between 1 and 10! Retry"+ColorUI.RESET);
+                    out.print(ColorUI.RED_TEXT+"You must choose a number between 1 and 10! Retry "+ColorUI.RESET);
                     x=in.nextInt();
                 }
                 tilesToPick.add(x);
             }else{
-                out.println("Chose the y coordinate of the tile that you want to pick: ");
+                out.print("Chose the y coordinate of the tile that you want to pick: ");
                 int y=in.nextInt();
                 //Check if the value is between 1 and 10
                 while(y<0 || y>11){
-                    out.println(ColorUI.RED_TEXT+"You must choose a number between 1 and 10! Retry"+ColorUI.RESET);
+                    out.print(ColorUI.RED_TEXT+"You must choose a number between 1 and 10! Retry "+ColorUI.RESET);
                     y=in.nextInt();
                 }
                 tilesToPick.add(y);
@@ -87,10 +87,10 @@ public class Cli{
     }
 
     public int askColumn(){
-        out.println("Choose the column to insert the tiles chosen: ");
+        out.print("Choose the column to insert the tiles chosen: ");
         int column=in.nextInt();
         while(column<1 || column>5){
-            out.println(ColorUI.RED_TEXT+"You must choose a number between 1 and 5! Retry"+ColorUI.RESET);
+            out.print(ColorUI.RED_TEXT+"You must choose a number between 1 and 5! Retry "+ColorUI.RESET);
             column= in.nextInt();
         }
         return (column-1);
