c5fee4afe389f0ee901da99a7bcd81aa799bf3ac
Arimndo <arimondo.scrivano@mail.polimi.it>
2023-05-27 11:00:08 +0200

    Game Controller test fix 1

--- src/test/java/controller/GameControllerTest.java
+++ src/test/java/controller/GameControllerTest.java
@@ -1,6 +1,10 @@
 package controller;
 
+import Network.RMI.Client_RMI;
+import Network.SOCKET.Sock;
+import Network.SOCKET.SocketClientHandler;
 import Network.Server;
+import Network.messages.MessageType;
 import junit.framework.TestCase;
 import model.Game;
 import model.Player;
@@ -8,8 +12,15 @@ import model.Shelf;
 import model.cgoal.CommonGoals;
 import org.junit.jupiter.api.Test;
 import model.Tile;
+import view.TextualUI;
+import view.View;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,137 +71,67 @@ class GameControllerTest extends TestCase {
 
     //TESTING METHOD somethingChanged()
     @Test
-    public void testCheckerSomethingChanged() throws RemoteException {
-        GameController myGC= new GameController(1, new Server(16001));
-        myGC.somethingChanged();
+    public void testCheckerSomethingChanged() throws IOException, NotBoundException {
+        Server server = new Server(16001);
+        try {
+        Registry registry = LocateRegistry.createRegistry(16000);
+        System.out.println("Server is booting....");
+        registry.rebind("server", server);
+        Thread thread = new Thread(server, "server");
+        thread.start();
+        System.out.println("Server created");
+    } catch (Exception e) {
+        System.out.println("Something went wrong :( " + e);
     }
 
-    //TESTING METHOD started()
-    @Test
-    public void testCheckerStarted() throws RemoteException {
-        GameController myGC= new GameController(4, new Server(16001));
-        myGC.createPlayer(0, "paolo");
-        myGC.createPlayer(1, "pietro");
-        myGC.createPlayer(2, "giovanni");
-        myGC.createPlayer(3, "tommaso");
-        myGC.started();
-        GameController otherGC= new GameController(2, new Server(16001));
-        otherGC.createPlayer(0, "paolo");
-        otherGC.createPlayer(1, "pietro");
-        otherGC.started();
-        GameController GC= new GameController(3, new Server(16001));
-        GC.createPlayer(0, "paolo");
-        GC.createPlayer(1, "pietro");
-        GC.createPlayer(2, "giovanni");
-        GC.started();
-    }
-
-    //TESTING METHOD ended()
-    @Test
-    public void testChecker() throws RemoteException {
-        GameController myGC= new GameController(3, new Server(16001));
-        myGC.ended();
-    }
-
-    //TESTING METHOD changeName(int id, String name)
-    @Test
-    public void testCheckerChangeName() throws RemoteException {
-        GameController myGC= new GameController(3, new Server(16001));
-        myGC.createPlayer(0, "Giovanni");
-        myGC.changeName(0, "Paolo");
-        myGC.createPlayer(1, "Giovanni");
-        myGC.createPlayer(2, "Tommaso");
-        myGC.changeName(2, "Pietro");
-    }
-
-    //TESTING METHOD finderPlayer(String name)
-    @Test
-    public void testCheckerFinderPlayer() throws RemoteException {
+        Client_RMI Franco = new Client_RMI("Franco");
+        server.createLobby(2, "Franco", Franco);
+        assertTrue(Franco.notifyMe().getMessageType().equals(MessageType.LOBBYCREATED));
+        Client_RMI Mario = new Client_RMI("Mario");
+        server.addPlayer(0,"Mario",Mario );
+        assertTrue(Franco.notifyMe().getMessageType().equals(MessageType.GAME_STARTING));
+        List<Integer> x= new ArrayList<>();
+        List<Integer> y= new ArrayList<>();
+        x.add(4);
+        y.add(2);
+        Franco.columnAvailable(1,1);
+        Franco.pickableTiles(x,y);
+        Franco.FinalPick(1, x,y);
+        assertTrue(Franco.notifyMe().getMessageType().equals(MessageType.SOMETHINGCHANGED));
+        //Inserting the tiles
+        x.set(0,5);
+        y.set(0,2);
+        Mario.insertTiles(x,y ,1);
+        //removing the tiles from the dashboard
+        Mario.FinalPick(1,x,y);
+        assertFalse(Franco.pickableTiles(x,y));
+        Mario.getDashboard();
+        Mario.getCommonGoals();
+        Mario.getMyShelfie();
+        Mario.getMyPersonalGoal();
+        Mario.myPoints();
+        Mario.myPGpoints();
+        assertFalse(Franco.columnAvailable(10,1));
+        assertFalse(Franco.notifyMe().getMessageType().equals(MessageType.GAME_ENDING));
+        Client_RMI Mario2 = new Client_RMI("Mario");
+        Client_RMI Mario3 = new Client_RMI("Mario");
+        server.createLobby(3, "Mario", Mario2);
+        server.addPlayer(1,"Mario",Mario3 );
+        assertTrue(Mario3.nameAlreadyTaken("Mario"));
+        Mario3.changeName("Luigi");
+        assertFalse(Mario3.notifyMe().getMessageType().equals(MessageType.GAME_STARTING));
+       /* TextualUI view = new TextualUI();
+        view.init();
+        ClientControllerV2 clientControllerV2 = new ClientControllerV2(view, "localhost", 16001);
+        //clientControllerV2.gameFlow();
         GameController myGC= new GameController(3, new Server(16001));
         myGC.createPlayer(0, "Paolo");
         int paoloIndex=myGC.finderPlayer("Paolo");
         int marcoIndex=myGC.finderPlayer("Marco");
-    }
 
-    //TESTING METHOD pickNextPlayer()
-    @Test
-    public void testCheckerPickNextPlayer() throws RemoteException {
-        GameController myGC=new GameController(3, new Server(16001));
-        myGC.createPlayer(0, "Giovannni");
-        myGC.createPlayer(1, "Rita");
-        myGC.createPlayer(2, "Paolo");
-        myGC.pickNextPlayer();
-    }
-
-    //TESTING METHOD playerTurn()
-    @Test
-    public void testCheckerPlayerTurn() throws RemoteException {
-        GameController myGC= new GameController(3, new Server(16001));
+        GameController myGC2= new GameController(3, new Server(16001));
         myGC.createPlayer(0, "Giovanni");
-        myGC.playerTurn();
-    }
-
-    //TESTING METHOD pickTiles(List<Integer> xCord, List<Integer> yCord)
-    @Test
-    public void testCheckerPickTiles() throws RemoteException {
-        GameController myGC= new GameController(3, new Server(16001));
-        List<Integer> xCord= new ArrayList<>();
-        xCord.add(1);
-        xCord.add(2);
-        List<Integer> yCord= new ArrayList<>();
-        yCord.add(1);
-        yCord.add(2);
-        myGC.pickTiles(xCord, yCord);
-
-    }
-
-    //TESTING METHOD tileAvailablePick(List<Integer> xCord, List<Integer> yCord)
-    @Test
-    //completare
-    public void testCheckerAvailablePick()throws RemoteException{
-        GameController myGC= new GameController(3, new Server(16001));
-        List<Integer> xCord= new ArrayList<>();
-        xCord.add(1);
-        xCord.add(2);
-        List<Integer> yCord= new ArrayList<>();
-        yCord.add(1);
-        yCord.add(2);
-        myGC.tileAvailablePick(xCord, yCord);
-        xCord.add(6);
-        xCord.add(6);
-        yCord.add(6);
-        yCord.add(5);
-        myGC.tileAvailablePick(xCord, yCord);
-    }
-
-    //TESTING METHOD insertTiles(List<Integer> xCord, List<Integer> yCord, int column)
-    @Test
-    public void testCheckerInsertTiles() throws RemoteException{
-        GameController myGC= new GameController(3, new Server(16001));
-        myGC.createPlayer(0, "giovanni");
-        myGC.createPlayer(1, "rita");
-        myGC.createPlayer(2, "emma");
-        List<Integer> xCord= new ArrayList<>();
-        xCord.add(1);
-        xCord.add(2);
-        List<Integer> yCord= new ArrayList<>();
-        yCord.add(1);
-        yCord.add(2);
-        myGC.insertTiles(xCord, yCord, 3);
-    }
-
-
-    //TESTING METHOD columnAvailable(int numTiles, Shelf myShelf, int column)
-    @Test
-    public void testCheckerColumnAvailable() throws RemoteException {
-        GameController myGC= new GameController(3, new Server(16001));
-        myGC.createPlayer(0, "paolo");
-        myGC.createPlayer(1, "pietro");
-        myGC.createPlayer(2, "giovanni");
-        Shelf myShelf=myGC.getPlayersList().get(0).getShelf();
-        myGC.columnAvailable(3, myShelf, 2);
-        myGC.columnAvailable(2, myShelf, 2);
-        myGC.columnAvailable(1, myShelf, 2);
+        myGC.playerTurn();*/
     }
 
     //TESTING METHOD checkPoints()
