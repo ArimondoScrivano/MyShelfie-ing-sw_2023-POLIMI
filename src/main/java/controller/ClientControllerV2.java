d1bb98b7cbb37b5310a38d5a0cbbbb77dc4fffdb
Arimndo <arimondo.scrivano@mail.polimi.it>
2023-06-04 23:06:07 +0200

    fix init game

--- src/main/java/controller/ClientControllerV2.java
+++ src/main/java/controller/ClientControllerV2.java
@@ -20,7 +20,6 @@ public class ClientControllerV2 {
     //Index of the Lobby
     private int idLobby;
 
-    private int flaginitgame;
 
     public int getIdLobby() {
         return idLobby;
@@ -37,7 +36,6 @@ public class ClientControllerV2 {
     public ClientControllerV2(View view, UI cli, String address, int port){
         this.view=view;
         this.cli= cli;
-        this.flaginitgame=0;
         try{
             this.client=new SocketClientV2(address, port, this);
             client.pingMessage(true);
@@ -69,18 +67,10 @@ public class ClientControllerV2 {
             case GAME_STARTING -> {
                 System.out.println("Game starting");
                 client.sendMessage(new Message(name, SocketMessages.IS_IT_MY_TURN, idLobby));
-                if(flaginitgame==0) {
-                    view.initGame();
-                    flaginitgame = 1;
-                }
             }
 
             case WAITING_FOR_OTHER_PLAYERS -> {
                 System.out.println("Waiting for other players");
-                if(flaginitgame==0) {
-                    view.initGame();
-                    flaginitgame = 1;
-                }
             }
 
             case WAITING_FOR_YOUR_TURN -> {
@@ -88,10 +78,7 @@ public class ClientControllerV2 {
             }
             case CHECK_YOUR_TURN -> {
                 client.sendMessage(new Message(name, SocketMessages.IS_IT_MY_TURN, idLobby));
-                if(flaginitgame==0) {
-                    view.initGame();
-                    flaginitgame = 1;
-                }
+
             }
             case MY_TURN->{
                 System.out.println(ColorUI.BLUE_TEXT+this.name+" is your turn!"+ColorUI.RESET);
