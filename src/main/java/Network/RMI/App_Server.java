1d602300808684c2e65bd485c464d814e8d43089
Lorenzo <lorenzo.vercelloni@mail.polimi.it>
2023-06-25 17:18:41 +0200

    Rmi registry update

--- src/main/java/Network/RMI/App_Server.java
+++ src/main/java/Network/RMI/App_Server.java
@@ -3,15 +3,24 @@ package Network.RMI;
 
 import Network.Server;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.RMISocketFactory;
 
 public class App_Server {
 
     public static void main(String [] args) {
         try {
+            System.setProperty("java.rmi.server.hostname", "172.16.0.13");
             //16000- RMI REGISTRY PORT
             Registry registry = LocateRegistry.createRegistry(16000);
+
             System.out.println("Server is booting....");
 
             //16001- SOCKET PORT
