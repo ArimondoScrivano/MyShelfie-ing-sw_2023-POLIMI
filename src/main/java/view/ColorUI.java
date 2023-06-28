1a9ac37c6f19938677c0f003dadd36a483c1f4bc
Arimndo <arimondo.scrivano@mail.polimi.it>
2023-06-28 10:54:59 +0200

    color changes

--- src/main/java/view/ColorUI.java
+++ src/main/java/view/ColorUI.java
@@ -6,25 +6,23 @@ package view;
  * It provides text colors and background colors for various elements.
  */
 public enum ColorUI {
-    //Text Colors
-    //Reset the colors
-    RESET("\u001B[00m"),
+    // Text Colors
+    // Reset the colors
+    RESET("\u001B[0m"),
     RED_TEXT("\u001B[31m"),
     BLUE_TEXT("\u001B[34m"),
     YELLOW_TEXT("\u001B[33m"),
-
     GREEN_TEXT("\u001B[32m"),
 
-    //Background colors
-    //Used for the tiles
+    // Background colors
+    // Used for the tiles
     BLANK_BG("\u001B[40m"),
-    GREEN_BG("\u001B[102m"),
-    YELLOW_BG("\u001B[103m"),
-    WHITE_BG("\u001B[107m"),
-    LIGHTBLUE_BG("\u001B[46;1m"),
-    VIOLET_BG("\u001B[105m"),
-    BLUE_BG("\u001B[104;1m");
-
+    GREEN_BG("\u001B[42m"),
+    YELLOW_BG("\u001B[43m"),
+    WHITE_BG("\u001B[47m"),
+    LIGHTBLUE_BG("\u001B[46m"),
+    VIOLET_BG("\u001B[45m"),
+    BLUE_BG("\u001B[44m");
     private final String code;
 
 
